4ee50f0444a2e34e0c0f5692ec737ab26cc27758
Tyler Buck <tyler.buck@maine.edu>
2015-02-23 15:09:03 -0500

    Separate the mainnav from the ddmenu module

--- modules/ddmenu/js/init.js
+++ modules/ddmenu/js/init.js
@@ -1,6 +1,24 @@
 /**
  * Drilldown menu init
  */
-// test log
-console.log("INIT - ddmenu");
+(function($) {
 
+    $('.mainnav.-root').apDrillDownMenu({
+        maxWindowWidth: 960, // Width that the navigation becomes "active"
+        width: '100%',
+        height: 'auto',
+        showSpeed: 200,
+        backLink: true,
+        backLinkText: 'Back',
+        backLinkSelector: '.ddmenu_back',
+        currentText: true,
+        currentTextSelector: '.ddmenu_current',
+        cloneClass: 'clone',
+        prependCurrentOnChild: true,
+        prependCurrentOnChildCallback: function() {},
+        parentIconText: '',
+        toggleSwitch: '.ddmenu',
+        toggleSpeed: 200
+    });
+
+})(jQuery);
