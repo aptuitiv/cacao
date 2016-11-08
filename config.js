1f7a2d7ca8f2ef859aa04318d22eb9d5689cde77
Eric Tompkins <eric@aptuitiv.com>
2016-11-08 08:23:49 -0500

    Code formatting

--- config.js
+++ config.js
@@ -5,63 +5,63 @@ var destDir = buildDest + siteRoot;
 
 module.exports = {
 
-  root: {
-    dest: buildDest,
-    src: srcDir,
-    server: siteRoot
-  },
-
-  copy: [
-    {
-      dest: destDir + '/layout/magnific',
-      src: ['node_modules/magnific-popup/dist/**/*']
+    root: {
+        dest: buildDest,
+        src: srcDir,
+        server: siteRoot
     },
-    {
-      dest: destDir + '/layout/slick',
-      src: ['node_modules/slick-carousel/slick/**/*']
-    }
-  ],
 
-  images: {
-    dest: destDir + '/layout/images',
-    src: srcDir + '/images/**/*.{png,jpg,gif}'
-  },
+    copy: [
+        {
+            dest: destDir + '/layout/magnific',
+            src: ['node_modules/magnific-popup/dist/**/*']
+        },
+        {
+            dest: destDir + '/layout/slick',
+            src: ['node_modules/slick-carousel/slick/**/*']
+        }
+    ],
 
-  nunjucks: {
-    dest: destDir,
-    src: [srcDir + '/site'],
-    pages: [srcDir + '/site/pages/**/*.html'],
-    data: [srcDir + '/site/data/**/*.json'],
-    watch: [srcDir + '/site/**/*.{html,json}']
-  },
-
-  scripts: [
-    {
-      name: 'index.js',
-      src: [
-        srcDir + '/scripts/modernizr-flexbox-detection.js',
-        'node_modules/jquery/dist/jquery.js',
-        'node_modules/ap_drilldown_plugin/ap-drilldown-menu.min.js',
-        srcDir + '/scripts/index.js'
-      ],
-      dest: destDir + '/layout/js'
+    images: {
+        dest: destDir + '/layout/images',
+        src: srcDir + '/images/**/*.{png,jpg,gif}'
     },
-    {
-      name: 'forms.js',
-      src: 'node_modules/jquery-validation/dist/jquery.validate.js',
-      dest: destDir + '/layout/js'
+
+    nunjucks: {
+        dest: destDir,
+        src: [srcDir + '/site'],
+        pages: [srcDir + '/site/pages/**/*.html'],
+        data: [srcDir + '/site/data/**/*.json'],
+        watch: [srcDir + '/site/**/*.{html,json}']
+    },
+
+    scripts: [
+        {
+            name: 'index.js',
+            src: [
+                srcDir + '/scripts/modernizr-flexbox-detection.js',
+                'node_modules/jquery/dist/jquery.js',
+                'node_modules/ap_drilldown_plugin/ap-drilldown-menu.min.js',
+                srcDir + '/scripts/index.js'
+            ],
+            dest: destDir + '/layout/js'
+        },
+        {
+            name: 'forms.js',
+            src: 'node_modules/jquery-validation/dist/jquery.validate.js',
+            dest: destDir + '/layout/js'
+        }
+    ],
+
+    styles: {
+        dest: destDir + '/layout/css',
+        src: [srcDir + '/styles/index.css'],
+        watch: [srcDir + '/styles/**/*.css']
+    },
+
+    stylelint: {
+        src: [srcDir + '/styles/**/*.css']
     }
-  ],
-
-  styles: {
-    dest: destDir + '/layout/css',
-    src: [srcDir + '/styles/index.css'],
-    watch: [srcDir + '/styles/**/*.css']
-  },
-
-  stylelint: {
-    src: [srcDir + '/styles/**/*.css']
-  }
 
 };
 
