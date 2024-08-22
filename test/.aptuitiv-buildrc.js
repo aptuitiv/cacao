c03d9f394147c53c0d04258a70391e593e998c70
Eric Tompkins <eric@aptuitiv.com>
2024-08-22 11:07:31 -0400

    Added test page to test the CSS

---
+++ test/.aptuitiv-buildrc.js
@@ -0,0 +1,14 @@
+/**
+ * Configuration for the aptuitiv-build package.
+ * See https://github.com/aptuitiv/website-build-tools/blob/main/docs/Configuration.md for more information.
+ */
+export default {
+    copy: [],
+    css: {
+        buildFiles: 'main.css'
+    },
+    javascript: {
+        bundles: [],
+        files: []
+    }
+};
