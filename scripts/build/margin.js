972c6b593d0482203a66a38fec1a9ec68460f5c3
Eric Tompkins <eric@aptuitiv.com>
2024-08-22 06:44:40 -0400

    Updated margin and padding to use the helper functions

--- scripts/build/margin.js
+++ scripts/build/margin.js
@@ -9,88 +9,29 @@ import fs from 'fs-extra';
 import fancyLog from 'fancy-log';
 import logSymbols from 'log-symbols';
 
-/**
- * Create the margin variables file
- */
-const createMarginVariables = () => new Promise((resolve) => {
-    const sizes = {
-        0: 0,
-        1: 10,
-        2: 20,
-        3: 30,
-        4: 40,
-        5: 50,
-        6: 60,
-        7: 70,
-        8: 80,
-        9: 90,
-        10: 100,
-        11: 120,
-        12: 160,
-        13: 240,
-        14: 320,
-        15: 480,
-    };
-    let fileContents = '/* =========================================================================== *\n';
-    fileContents += '  Variables for the margin sizes\n';
-    fileContents += ' * =========================================================================== */\n\n';
-    fileContents += '/* :where() is used to give the variables no specificity so that they are easily overriden. https://developer.mozilla.org/en-US/docs/Web/CSS/:where */';
-    fileContents += '\n\n';
-    fileContents += ':where(html) {';
-    Object.keys(sizes).forEach((i) => {
-        fileContents += '\n';
-        fileContents += `    --margin-${i}: ${sizes[i]}px;`;
-    });
-    fileContents += '\n}';
-    fs.writeFileSync('src/margin/variables.css', fileContents);
-    fancyLog(chalk.green(`${logSymbols.success} Wrote margin variables file `, chalk.cyan('src/margin/variables.css')));
-    resolve();
-});
+import { variableSizes } from './config.js';
+import { buildModuleCombinationFile, buildModuleSideFileContent, createModuleVariables } from './helpers.js';
 
 /**
  * Build the margin class files
  */
 const buildMarginsFiles = () => new Promise((resolve) => {
     const sides = {
-        all: { class: 'm', comment: 'All sides margin utilities', dec: 'margin' },
-        bottom: { class: 'mb', comment: 'Bottom margin utilities', dec: 'margin-bottom' },
-        end: { class: 'me', comment: 'End margin utilities', dec: 'margin-right' },
-        horizontal: { class: 'mx', comment: 'Horizontal margin utilities', dec: ['margin-left', 'margin-right'] },
-        start: { class: 'ms', comment: 'Start margin utilities', dec: 'margin-left' },
-        top: { class: 'mt', comment: 'Top margin utilities', dec: 'margin-top' },
-        vertical: { class: 'my', comment: 'Vertical margin utilities', dec: ['margin-top', 'margin-bottom'] },
+        all: { class: 'm', comment: 'All sides margin utilities', property: 'margin' },
+        bottom: { class: 'mb', comment: 'Bottom margin utilities', property: 'margin-bottom' },
+        end: { class: 'me', comment: 'End margin utilities', property: 'margin-right' },
+        horizontal: { class: 'mx', comment: 'Horizontal margin utilities', property: ['margin-left', 'margin-right'] },
+        start: { class: 'ms', comment: 'Start margin utilities', property: 'margin-left' },
+        top: { class: 'mt', comment: 'Top margin utilities', property: 'margin-top' },
+        vertical: { class: 'my', comment: 'Vertical margin utilities', property: ['margin-top', 'margin-bottom'] },
     };
     Object.keys(sides).forEach((side) => {
-        let fileContents = '/* =========================================================================== *\n';
-        fileContents += `   ${sides[side].comment}\n`;
-        fileContents += ' * =========================================================================== */\n';
-
-        for (let i = 0; i <= 15; i += 1) {
-            fileContents += '\n\n';
-            fileContents += `.${sides[side].class}-${i} {\n`;
-            const { dec } = sides[side];
-            if (Array.isArray(dec)) {
-                const declarations = dec.map((d) => `    ${d}: var(--margin-${i});\n`);
-                fileContents += declarations.join('');
-            } else {
-                fileContents += `    ${dec}: var(--margin-${i});\n`;
-            }
-            fileContents += '}';
-        }
-        fileContents += '\n';
+        const fileContents = buildModuleSideFileContent('margin', sides[side]);
         fs.writeFileSync(`src/margin/${side}.css`, fileContents);
         fancyLog(chalk.green(`${logSymbols.success} Wrote margin file `, chalk.cyan(`src/margin/${side}.css`)));
     });
 
-    // Create the combination file
-    let fileContents = '/* =========================================================================== *\n';
-    fileContents += '   Margin utilities - imports all the margin utility files\n';
-    fileContents += ' * =========================================================================== */\n\n';
-    Object.keys(sides).forEach((side) => {
-        fileContents += `@import './${side}.css';\n`;
-    });
-    fs.writeFileSync('src/margin/margin.css', fileContents);
-    fancyLog(chalk.green(`${logSymbols.success} Wrote margin file `, chalk.cyan('src/margin/margin.css')));
+    buildModuleCombinationFile('margin', sides);
     fancyLog(chalk.green(`${logSymbols.success} Done creating margin files `));
     resolve();
 });
@@ -101,9 +42,11 @@ const buildMarginsFiles = () => new Promise((resolve) => {
  * @returns {Promise<void>}
  */
 const buildMargins = async () => new Promise((resolve) => {
-    createMarginVariables();
-    buildMarginsFiles();
-    resolve();
+    createModuleVariables('margin', variableSizes).then(() => {
+        buildMarginsFiles().then(() => {
+            resolve();
+        });
+    });
 });
 
 export default buildMargins;
--- scripts/build/padding.js
+++ scripts/build/padding.js
@@ -9,88 +9,29 @@ import fs from 'fs-extra';
 import fancyLog from 'fancy-log';
 import logSymbols from 'log-symbols';
 
-/**
- * Create the padding variables file
- */
-const createPaddingVariables = () => new Promise((resolve) => {
-    const sizes = {
-        0: 0,
-        1: 10,
-        2: 20,
-        3: 30,
-        4: 40,
-        5: 50,
-        6: 60,
-        7: 70,
-        8: 80,
-        9: 90,
-        10: 100,
-        11: 120,
-        12: 160,
-        13: 240,
-        14: 320,
-        15: 480,
-    };
-    let fileContents = '/* =========================================================================== *\n';
-    fileContents += '  Variables for the padding sizes\n';
-    fileContents += ' * =========================================================================== */\n\n';
-    fileContents += '/* :where() is used to give the variables no specificity so that they are easily overriden. https://developer.mozilla.org/en-US/docs/Web/CSS/:where */';
-    fileContents += '\n\n';
-    fileContents += ':where(html) {';
-    Object.keys(sizes).forEach((i) => {
-        fileContents += '\n';
-        fileContents += `    --padding-${i}: ${sizes[i]}px;`;
-    });
-    fileContents += '\n}';
-    fs.writeFileSync('src/padding/variables.css', fileContents);
-    fancyLog(chalk.green(`${logSymbols.success} Wrote padding variables file `, chalk.cyan('src/padding/variables.css')));
-    resolve();
-});
+import { variableSizes } from './config.js';
+import { buildModuleCombinationFile, buildModuleSideFileContent, createModuleVariables } from './helpers.js';
 
 /**
  * Build the padding class files
  */
 const buildPaddingsFiles = () => new Promise((resolve) => {
     const sides = {
-        all: { class: 'p', comment: 'All sides padding utilities', dec: 'padding' },
-        bottom: { class: 'pb', comment: 'Bottom padding utilities', dec: 'padding-bottom' },
-        end: { class: 'pe', comment: 'End padding utilities', dec: 'padding-right' },
-        horizontal: { class: 'px', comment: 'Horizontal padding utilities', dec: ['padding-left', 'padding-right'] },
-        start: { class: 'ps', comment: 'Start padding utilities', dec: 'padding-left' },
-        top: { class: 'pt', comment: 'Top padding utilities', dec: 'padding-top' },
-        vertical: { class: 'py', comment: 'Vertical padding utilities', dec: ['padding-top', 'padding-bottom'] },
+        all: { class: 'p', comment: 'All sides padding utilities', property: 'padding' },
+        bottom: { class: 'pb', comment: 'Bottom padding utilities', property: 'padding-bottom' },
+        end: { class: 'pe', comment: 'End padding utilities', property: 'padding-right' },
+        horizontal: { class: 'px', comment: 'Horizontal padding utilities', property: ['padding-left', 'padding-right'] },
+        start: { class: 'ps', comment: 'Start padding utilities', property: 'padding-left' },
+        top: { class: 'pt', comment: 'Top padding utilities', property: 'padding-top' },
+        vertical: { class: 'py', comment: 'Vertical padding utilities', property: ['padding-top', 'padding-bottom'] },
     };
     Object.keys(sides).forEach((side) => {
-        let fileContents = '/* =========================================================================== *\n';
-        fileContents += `   ${sides[side].comment}\n`;
-        fileContents += ' * =========================================================================== */\n';
-
-        for (let i = 0; i <= 15; i += 1) {
-            fileContents += '\n\n';
-            fileContents += `.${sides[side].class}-${i} {\n`;
-            const { dec } = sides[side];
-            if (Array.isArray(dec)) {
-                const declarations = dec.map((d) => `    ${d}: var(--padding-${i});\n`);
-                fileContents += declarations.join('');
-            } else {
-                fileContents += `    ${dec}: var(--padding-${i});\n`;
-            }
-            fileContents += '}';
-        }
-        fileContents += '\n';
+        const fileContents = buildModuleSideFileContent('padding', sides[side]);
         fs.writeFileSync(`src/padding/${side}.css`, fileContents);
         fancyLog(chalk.green(`${logSymbols.success} Wrote padding file `, chalk.cyan(`src/padding/${side}.css`)));
     });
 
-    // Create the combination file
-    let fileContents = '/* =========================================================================== *\n';
-    fileContents += '   Padding utilities - imports all the padding utility files\n';
-    fileContents += ' * =========================================================================== */\n\n';
-    Object.keys(sides).forEach((side) => {
-        fileContents += `@import './${side}.css';\n`;
-    });
-    fs.writeFileSync('src/padding/padding.css', fileContents);
-    fancyLog(chalk.green(`${logSymbols.success} Wrote padding file `, chalk.cyan('src/padding/padding.css')));
+    buildModuleCombinationFile('padding', sides);
     fancyLog(chalk.green(`${logSymbols.success} Done creating padding files `));
     resolve();
 });
@@ -101,9 +42,11 @@ const buildPaddingsFiles = () => new Promise((resolve) => {
  * @returns {Promise<void>}
  */
 const buildPaddings = async () => new Promise((resolve) => {
-    createPaddingVariables();
-    buildPaddingsFiles();
-    resolve();
+    createModuleVariables('padding', variableSizes).then(() => {
+        buildPaddingsFiles().then(() => {
+            resolve();
+        });
+    });
 });
 
 export default buildPaddings;
