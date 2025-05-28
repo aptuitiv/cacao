8b2caac147ef234704924ddd3353d7ff4ec36c67
Eric Tompkins <eric@aptuitiv.com>
2025-05-28 17:49:47 -0400

    Removed commented out code

--- eslint.config.js
+++ eslint.config.js
@@ -21,19 +21,3 @@ export default defineConfig([
         },
     },
 ]);
-
-
-/**
- * This is used with linting files in this package.
- */
-// module.exports = {
-//     "env": {
-//         "es2024": true,
-//         "node": true,
-//     },
-//     extends: ['@aptuitiv/eslint-config-aptuitiv'],
-//     rules: {
-//         // Allow importing Javascript files
-//         'import/extensions': 'off',
-//     }
-// }
